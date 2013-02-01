6c0d576d5f9668c16bcf54c144d8459a8b5fbb6c
szvinsky <s.zvinsky@live.com>
2013-02-01 07:28:51 -0500

    template engine examples

---
+++ examples/template.simple.js
@@ -0,0 +1,21 @@
+var util = require('util');
+var ferrum = require('../lib/index');
+
+function MainHandler () {
+    ferrum.RequestHandler.call(this);
+    
+    this.get = function () {
+        this.render('index', { "variable": "test variable" });
+    };
+}
+
+util.inherits(MainHandler, ferrum.RequestHandler);
+
+ferrum.Application({
+  port: process.env.PORT,
+  host: process.env.IP,
+  views: './examples/views',
+  routes: {
+    '^/$': MainHandler
+  }
+}).run();
